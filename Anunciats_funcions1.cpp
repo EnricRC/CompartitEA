d935769a5d39f694271c648a018bd0c3c79e3bd4
alp24032 <84396222+alp24032@users.noreply.github.com>
2021-09-29 15:56:20 +0200

    Pujat fitxer
    He pujat un fitxer del M03

---
+++ Anunciats_funcions1.cpp
@@ -0,0 +1,46 @@
+//============================================================================
+// Name        : Anunciats_funcions1.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+/*
+1. (funcions11) Fes una funció que escrigui “hola, com va la vida” per pantalla. La funció
+serà de tipus void, per tant no retorna res i no rep cap paràmetre. Comprova el seu
+funcionament.
+
+2. (funcions12) Fes una funció a la qual li passis com a paràmetre un nom, per exemple
+Pepe, i ens mostri per pantalla “hola Pepe”. La funció no retorna res, per tant serà
+void. Comprova el seu funcionament.
+
+3. (funcions13) Fes una funció a la qual li passem un número per paràmetre i ens retorna
+el doble d’aquest número. La funció es dirà doble i serà del tipus int ja que ens
+retornarà un enter. Comprova el seu funcionament.
+
+4. (funcions14) Fes una funció a la qual li passem dos números i mostra per pantalla la
+seva suma. Els dos paràmetres que li passarem seran del tipus int i, com que la funció
+no retornarà res ha de ser void. Comprova el seu funcionament.
+
+5. (funcions15) Fes una funció a la qual li passem dos números i ens retorna la seva
+suma. Els dos paràmetres que li passarem seran del tipus int i el retorn de la funció ha
+de ser int també. Comprova el seu funcionament.
+
+6. (funcions16) Fes una funció a la qual li passem un número i ens diu si el número és
+primer o no. El paràmetre que li passem serà de tipus int i la funció retornarà un valor
+booleà. Comprova el seu funcionament.
+
+7. (funcions17) Fes una funció que mostri un menú del tipus:
+1. Altes
+2. Baixes
+3. Modificacions
+4. Consultes
+I que retorni un valor numèric a partir de l’opció triada. La funció no ha de rebre cap
+paràmetre.
+
+8. (funcions18) Aquesta funció és totalment diferent a les anteriors, ja que és el propi
+main qui rep paràmetres. Fes un programa principal main que rebi per paràmetre dos
+nombres i ens digui quina és la suma. Si no se li passen dos valors ens ha de dir que els
+paràmetres són incorrectes.
+ */
